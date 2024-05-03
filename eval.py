196787d8968bba0e05d12eacbac973362facceb6
Wilson McDade <wilson.mcdade@gmail.com>
2024-05-03 11:18:00 -0400

    Added experiment to eval.py

--- eval.py
+++ eval.py
@@ -9,7 +9,10 @@ import argparse
 import json
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
+import random
 
 import pytrec_eval as pytrec
 
@@ -19,8 +22,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__),'TILDE'))
 from TILDE.inference import inference
 from TILDE.tools import load_queries, load_run
 
-def build_run_name(alpha, is_long, run_path="TILDE/data/"):
-    return run_path+"runs/alpha{0}_{1}.txt".format(alpha, "Long" if is_long else "Short")
+def build_run_name(alpha, set_name, run_path="TILDE/data/"):
+    return run_path+"runs/alpha{0}_{1}.txt".format(alpha, set_name)
 
 def evaluate(run_file_path, qrel_path, metrics):
     with open(run_file_path) as f_run:
@@ -33,47 +36,90 @@ def evaluate(run_file_path, qrel_path, metrics):
 
     return evaluator.evaluate(run)
 
+def make_graphs(results):
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+    for label, grp in results.groupby('Set'):
+        plt.plot(grp['Alpha'], grp['MAP@10'], label=f'Set {label}', marker='o')
+    plt.title('Line Graph of MAP@10')
+    plt.xlabel('Alpha')
+    plt.ylabel('MAP@10')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+    for label, grp in results.groupby('Set'):
+        plt.plot(grp['Alpha'], grp['NDCG@10'], label=f'Set {label}', marker='o')
+    plt.title('Line Graph of NDCG@10')
+    plt.xlabel('Alpha')
+    plt.ylabel('NDCG@10')
+    plt.legend()
+
+    plt.savefig('results.png')
+
+    plt.tight_layout()
+    plt.show()
+
 # Run inference with given dependent variables
 #   alpha:     alpha value to use in range [0,1]
 #   queries:   set of queries to use
-def perform_inference(queries, run, alpha, is_long):
-    inference(queries, run, alpha, save_path=build_run_name(alpha, is_long))
+def perform_inference(queries, run, alpha, set_name):
+    inference(queries, run, alpha, save_path=build_run_name(alpha, set_name))
 
 def main(args):
 
+    df_columns = ["MAP@10", "NDCG@10", "Alpha", "Set"]
+    results = pd.DataFrame(columns = df_columns)
+
     queries = load_queries(args.run_path+"queries/DL2019-queries.tsv")
 
-    long_queries = {k: v for k, v in queries.items() if len(v.split()) > args.n}
-    short_queries = {k: v for k, v in queries.items() if len(v.split()) <= args.n}
+    # Split queries into named sets
+    query_set_a = {k: v for k, v in queries.items() if len(v.split()) <= 4}
+    query_set_b = {k: v for k, v in queries.items() if len(v.split()) == 5}
+    query_set_c = {k: v for k, v in queries.items() if len(v.split()) == 6}
+    query_set_d = {k: v for k, v in queries.items() if len(v.split()) > 6}
+
+    # Pick random query out of set
+    rand_query_a = random.choice(list(query_set_a.values()))
+    rand_query_b = random.choice(list(query_set_b.values()))
+    rand_query_c = random.choice(list(query_set_c.values()))
+    rand_query_d = random.choice(list(query_set_d.values()))
+
+    query_sets = [query_set_a, query_set_b, query_set_c, query_set_d]
+    query_sets_names = ["A", "B", "C", "D"]
 
     run = load_run(args.run_path+"runs/run.trec2019-bm25.res")
 
-    perform_inference(short_queries, run, args.alpha, False)
+    i = 0
+    for query_set in query_sets:
 
-    eval = evaluate(run_file_path = build_run_name(args.alpha, False),
-             qrel_path =     args.run_path+"qrels/2019qrels-pass.txt", 
-             metrics =       {'map_cut.10', 'ndcg_cut.10'}).values()
+        for alpha in np.arange(0, 1.1, step=0.1):
+            
+            perform_inference(query_set, run, alpha, query_sets_names[i])
 
-    short_df = pd.DataFrame(list(eval))
-    print("Short Queries:")
-    print(short_df.describe())
+            eval = evaluate(run_file_path = build_run_name(alpha, query_sets_names[i]),
+                    qrel_path =     args.run_path+"qrels/2019qrels-pass.txt", 
+                    metrics =       {'map_cut.10', 'ndcg_cut.10'}).values()
 
-    perform_inference(long_queries, run, (1-args.alpha), True)
+            result = pd.DataFrame(list(eval))
+            print("Set " + query_sets_names[i] + ":")
+            print(result.describe())
 
-    eval = evaluate(run_file_path = build_run_name((1-args.alpha), True),
-             qrel_path =     args.run_path+"qrels/2019qrels-pass.txt", 
-             metrics =       {'map_cut.10', 'ndcg_cut.10'}).values()
+            results = pd.concat([pd.DataFrame([[result["map_cut_10"].mean(), 
+                                               result["ndcg_cut_10"].mean(), 
+                                               alpha, 
+                                               query_sets_names[i]]], 
+                                               columns=df_columns), results], ignore_index=True)
 
-    long_df = pd.DataFrame(list(eval))
-    print("Long Queries:")
-    print(long_df.describe())
+            print(results.head())
+
+        i += 1
+
+    make_graphs(results)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--run_path", type=str, default="TILDE/data/")
-    parser.add_argument("--verbose", type=bool, default=False)
-    parser.add_argument("--alpha", type=float, default=0.4)
-    parser.add_argument("--n", type=int, default=5) 
     args = parser.parse_args()
 
     main(args)
